d3bfdbe85e39e97e9297cf835aee85785f26d63d
Mikko Rauhala <mikko@rauhala.net>
2017-06-24 06:23:06 +0300

    Added yum-utils package installation

--- smartmet-install.sh
+++ smartmet-install.sh
@@ -7,6 +7,8 @@ echo "Install EPEL repository" && \
 rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm && \
 echo "Install PostgreSQL repository" && \
 rpm -Uvh https://download.postgresql.org/pub/repos/yum/9.3/redhat/rhel-7-x86_64/pgdg-centos93-9.3-3.noarch.rpm && \
+echo "Install yum-utils package" && \
+yum -y install yum-utils && \
 echo "Install Docker CE repository" && \
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo && \
 echo "Install SmartMet packages" && \

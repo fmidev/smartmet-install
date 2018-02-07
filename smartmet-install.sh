c1d5e277c3d7c14520e0be1c2057a7ebe0375e8c
Mikko Rauhala <mikko@rauhala.net>
2018-02-08 00:01:33 +0200

    Updated pgdg repository and one dependency fix

--- smartmet-install.sh
+++ smartmet-install.sh
@@ -6,11 +6,13 @@ rpm -Uvh http://download.weatherproof.fi/fmiforge/rhel/7/noarch/fmiforge-release
 echo "Install EPEL repository" && \
 rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm && \
 echo "Install PostgreSQL repository" && \
-rpm -Uvh https://download.postgresql.org/pub/repos/yum/9.3/redhat/rhel-7-x86_64/pgdg-centos93-9.3-3.noarch.rpm && \
+rpm -Uvh https://download.postgresql.org/pub/repos/yum/9.6/redhat/rhel-7-x86_64/pgdg-centos96-9.6-3.noarch.rpm && \
 echo "Install yum-utils package" && \
 yum -y install yum-utils && \
 echo "Install Docker CE repository" && \
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo && \
+echo "Install postgis" && \
+yum -y install postgis-2.0* && \
 echo "Install SmartMet packages" && \
 yum -y install smartmet-base-international && \
 echo "Update packages" && \

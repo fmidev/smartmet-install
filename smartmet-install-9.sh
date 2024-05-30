006a58206f78562e9f74c531954353ada6b23261
Mikko Rauhala <mikko@rauhala.net>
2024-05-30 11:29:46 +0500

    Create smartmet-install-9.sh

---
+++ smartmet-install-9.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+echo "Install SmartMet Open repository" && \
+dnf -y install https://download.fmi.fi/smartmet-open/rhel/8/x86_64/smartmet-open-release-latest-9.noarch.rpm && \
+echo "Install yum utils" && \
+dnf -y install yum-utils && \
+echo "Install Docker CE repository" && \
+dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo && \
+echo "Enable PowerTools" && \
+dnf config-manager --set-enabled powertools && \
+echo "Install EPEL repository" && \
+dnf -y install epel-release && \
+echo "Disable eccodes from EPEL" && \
+dnf config-manager --setopt="epel.exclude=eccodes*" --save && \
+dnf config-manager --set-disabled epel-source && \
+echo "Disable postgresql:15 module" && \
+dnf -y module disable postgresql:15 && \
+echo "Install SmartMet packages" && \
+dnf -y install smartmet-base-international && \
+echo "Update packages" && \
+yum -y update
